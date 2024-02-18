6ab54eeb3daca510d3f61922e61f680e5ff889ce
TheWandererPT <franciscojoseferreiramartins@gmail.com>
2024-02-18 00:43:13 +0000

    all set

--- Assets/cenas/ProjectileGun.cs
+++ Assets/cenas/ProjectileGun.cs
@@ -121,8 +121,6 @@ public class ProjectileGun : MonoBehaviour
         }
 
         
-        if (bulletsShot < bulletsPerTap && bulletsLeft > 0)
-            Invoke("Shoot", timeBetweenShots);
     }
 
 
